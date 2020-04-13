fc7f6c2aac3a5f18b2185bdfb7db780453cb24b1
tiger20111 <nikit-proc@mail.ru>
2020-04-13 17:35:57 +0300

    Added names commit

--- shooter_version1/cube/cube.cpp
+++ shooter_version1/cube/cube.cpp
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
 GLuint getCubeVertex() {
